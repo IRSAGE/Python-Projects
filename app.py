d7130353ed619c0fbe90cec164df2dc3efe5bb6f
IR~SAGE <sibo.egide98@gmail.com>
2021-04-05 10:43:51 +0200

    Initial Commit

---
+++ app.py
@@ -0,0 +1,18 @@
+print("Hello World")
+print("*" * 10)
+# x = input("Enter a number")
+# print(x)
+# commenting
+
+temperature = 35
+if temperature > 30:
+    print("Its warm")
+    print("drink water")
+elif temperature == 35:
+    print("Its 30 degres")
+else:
+    print("No Temperature")
+print("done")
+# for loop
+for number in range(1, 10, 2):
+    print("Attempt", number, number * ".")
